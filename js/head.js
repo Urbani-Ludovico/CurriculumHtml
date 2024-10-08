a09656bca71a163c1a988bf633b65d529bedfe68
Ludovico Urbani <lu.urbani@outlook.com>
2024-10-08 08:51:07 +0200

    Fixed style import Added css definitions

--- js/head.js
+++ js/head.js
@@ -6,7 +6,9 @@ function headInit({
     const head = document.getElementsByTagName("head")[0];
 
     let content = "<meta charset=\"UTF-8\">" +
-        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0\" />";
+        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0\" />" +
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/styles/base.css\">" +
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/styles/std.css\">";
 
     if (!!title) {
         content += "<title>" + title + " | Urbani Ludovico - Curriculum</title>"
